3713eac106db5a0133c5ed6aeff0558185d3b1ae
zack1111111111111 <zj28@njit.edu>
2026-05-09 08:22:33 +0000

    Deploying to gh-pages from @ zack1111111111111/zack1111111111111.github.io@5a10b070dde59c9cd243e08d56283e3fdb2a4e34 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "PhD Candidate · Wearable Biosignal Systems · AI-Driven Human-Machine Interfaces.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
